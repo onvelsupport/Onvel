de57cfb0973682aca126d307900c30b0593b808e
Sa22akr <sa22akr@herts.ac.uk>
2026-04-20 17:55:16 +0100

    connected cloudinary for image  storage

--- onvel/settings.py
+++ onvel/settings.py
@@ -89,6 +89,7 @@ else:
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
